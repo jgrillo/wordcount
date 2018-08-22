0e320f6790767fec50ad76ea7b3cd12c81589aef
StabbyCutyou <smattkelly@gmail.com>
2018-08-21 21:56:52 -0400

    Small cleanup, added benchmarks

--- wordcount_test.go
+++ wordcount_test.go
@@ -2,12 +2,36 @@ package main
 
 import "testing"
 
+type testCase struct {
+	name  string
+	words words
+}
+
+var cases = []testCase{
+	{name: "4 words", words: words{[]string{"word", "word", "word", "wat"}}},
+	{name: "8 words", words: words{[]string{"word", "word", "word", "wat", "word", "word", "word", "wat"}}},
+	{name: "16 words", words: words{[]string{"word", "word", "word", "wat", "word", "word", "word", "wat", "word", "word", "word", "wat"}}},
+}
+
 func TestCountWords(t *testing.T) {
 	wds := words{[]string{"word", "word", "word", "wat"}}
 	result := countWords(wds)
 
-	if result.counts["word"] != 3 || result.counts["wat"] != 1 {
+	if result.Counts["word"] != 3 || result.Counts["wat"] != 1 {
 		t.Errorf("unexpected counts \"word\": %d \"wat\": %d",
-			result.counts["word"], result.counts["wat"])
+			result.Counts["word"], result.Counts["wat"])
+	}
+}
+
+func BenchmarkCountWords(b *testing.B) {
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				// Sometimes the compiler will think it can optimize calls
+				// it thinks are no-ops entirely out of the asm, but this one
+				// doesn't seem to so it's fine
+				countWords(c.words)
+			}
+		})
 	}
 }
